b28ed7a03ef547b9badb24301a98aaa0534b03da
짜몽 <teentoplove23@gmail.con>
2026-07-15 12:53:12 +0900

    commit

--- src/main/java/com/example/boot/controller/UserController.java
+++ src/main/java/com/example/boot/controller/UserController.java
@@ -2,9 +2,11 @@ package com.example.boot.controller;
 
 import com.example.boot.dto.UserDTO;
 import com.example.boot.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +29,14 @@ public class UserController {
     }
 
     @GetMapping("/login")
-    public void login(){
+    public void login(HttpServletRequest request, Model model){
+        String errMsg = (String)request.getSession().getAttribute("errMsg");
+        if(errMsg != null){
+            log.info(">> errMsg >>{}", errMsg);
+            model.addAttribute("errMsg", errMsg);
+        }
+        // 전송후 지우기
+        request.getSession().removeAttribute("errMsg");
 
     }
 }
--- src/main/java/com/example/boot/security/LoginFailuerHandler.java
+++ src/main/java/com/example/boot/security/LoginFailuerHandler.java
@@ -3,14 +3,42 @@ package com.example.boot.security;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 import java.io.IOException;
 
+@Slf4j
 public class LoginFailuerHandler implements AuthenticationFailureHandler {
+
+    // redirect 데이터 정보를 가지고 있는 객체 (redirect 로 경로 저장(이동) 역할)
+    RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        // 로그인 실패시
+        // error 메세지 띄우기
+        String errorMessage; // error message 저장용
 
+        // BadCredentialsException (패스워트 틀렸을 때)
+        // InternalAuthenticationServiceException (내부 시스템 문제)
+        // UsernameNotFoundException (id 오류)
+        // AuthenticationCredentialsNotFoundException (인증요청 거부)
+        if(exception instanceof BadCredentialsException){
+            errorMessage = "email / password fail";
+        }else {
+            errorMessage = "관리자에게 문의해주세요.";
+        }
+
+        log.info(">> error message >>{}", errorMessage);
+
+        // 로그인 페이지로 해당 에러 메시지를 리다이렉트
+        request.getSession().setAttribute("errMsg", errorMessage);
+
+        redirectStrategy.sendRedirect(request, response, "/user/login");
     }
 }
